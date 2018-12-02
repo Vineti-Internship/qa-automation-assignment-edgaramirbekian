98b838918b7c99a7691bfcd0db379d263af27494
Edgar <amirbekianedgar@gmail.com>
2018-12-02 22:12:35 +0400

    find tablets that cost less than 70$ and push it to array then print it to the console

--- test/specs/flow/test.js
+++ test/specs/flow/test.js
@@ -10,6 +10,16 @@ describe('Amazon Search', () => {
             browser.waitForExist(searchtab_xpath, 100000);
             browser.setValue(searchtab_xpath, 'tablet');
             browser.element(searchbutton_xpath).click();
+        })
+
+        it('Set Parameter under 70$', () =>{
+            let search_tablets = [];
+            browser.element('//*[@id="sort"]').selectByValue('price-asc-rank');
+            browser.waitForExist('//div');
+            if (browser.element('//div[@class="a-fixed-left-grid-col a-col-right"]//span[@class="sx-price-whole"]').getText() <= '70') {
+                search_tablets.push(browser.element('//h2[@class="a-size-medium s-inline  s-access-title  a-text-normal"]').getText());
+            }
+            console.log(search_tablets);
         }) 
 
     })
